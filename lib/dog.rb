ba0df9d9903abd9e2ef37d0310e50fa2afc5e378
adeeba reaz <adeebarivera@outlook.com>
2020-08-17 22:32:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,27 @@
-Class Dog 
-@@all = []
+class Dog
+  attr_accessor :name
 
- attr_accessor :name
- 
+  @@all = []
+
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
-  
-   def self.all
+
+  def self.all
     @@all
   end
+
+  def self.print_all
+    # puts @@all.map(&:name) - another way to solve, but more complex than needed for this example
+    puts @@all.map{ |dog| dog.name }
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
 end
-
-
-fido = dog.new("fido")
-purpose = Song.new("purpose")
